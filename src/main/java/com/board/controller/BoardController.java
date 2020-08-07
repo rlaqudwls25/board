6bd8af293c54ae0a47e76325e1e3e960cd81bfdd
rlaqudwls25 <rlaqudwls157@gmail.com>
2020-08-07 16:57:56 +0900

    ..

--- src/main/java/com/board/controller/BoardController.java
+++ src/main/java/com/board/controller/BoardController.java
@@ -12,7 +12,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Map;
 
 
 @Controller
--- src/main/webapp/WEB-INF/list.jsp
+++ src/main/webapp/WEB-INF/list.jsp
@@ -7,7 +7,7 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Write</title>
+    <title>List</title>
 </head>
 <style>
     h2 {
